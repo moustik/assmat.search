1c16f4e6bb0e80196511e47d8bb534442eac4bb7
moustik <damien.letournel@gmail.com>
2020-05-09 07:34:14 +0200

    Creation de fonction et documentation pour plus de clareté. Ajout d'un cache pour le geocodage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import optparse
+import hashlib
+import pickle
 
 import pandas
 import camelot
@@ -9,6 +11,144 @@ import geopy.extra.rate_limiter
 import folium
 
 from flask import Flask
+
+GEOCODE_CACHE = {}
+GEOCODE_CACHE_FILE = "geocode_cache.csv"
+
+
+def hasher(s):
+    return hashlib.md5(s.encode("utf-8")).hexdigest()
+
+
+def pull_cache():
+    try:
+        with open(GEOCODE_CACHE_FILE, "rb") as f:
+            return pickle.load(f)
+    except:
+        print("no cache found for geocodes")
+        return {}
+
+
+def save_cache():
+    with open(GEOCODE_CACHE_FILE, 'wb') as f:  # Just use 'w' mode in 3.x
+        pickle.dump(GEOCODE_CACHE, f)
+
+
+def fetch_geocode(address, provider_geocode, use_cache=True):
+    """Fetch geocode from address. Will build a cache by default to respect the
+    providers servers.
+
+    """
+    hash_address = hasher(address)
+    if use_cache and hash_address in GEOCODE_CACHE:
+        return GEOCODE_CACHE.get(hash_address)
+    else:
+        print("fetching {}".format(address))
+        point = None
+        location = provider_geocode(address)
+        if location:
+            point = tuple(location.point)
+        GEOCODE_CACHE[hash_address] = point
+        return point
+
+
+def add_geocode_to_dataset(dataset, provider, use_cache=True):
+    geocode = geopy.extra.rate_limiter.RateLimiter(provider.geocode,
+                                                   min_delay_seconds=.1)
+
+    return dataset.apply(lambda row: fetch_geocode(
+        "{} {}".format(row["Adresse"], row["Ville"]), geocode, use_cache),
+                         axis=1)
+
+
+def prepare_data_from_pdf(pdf_filename, csv_filename=None):
+    """
+
+    """
+    headers_pdf = ['Nom', 'Prenom', 'Adresse', 'Tel']
+
+    tables = camelot.read_pdf(pdf_filename, pages='all')
+
+    # assemble tables from different pages as one.
+    assembled_table = pandas.DataFrame()
+    for table in tables[1:]:
+        tmp = pandas.DataFrame(table.df)
+        if assembled_table.size == 0:  # we are dealing with the very first table
+            tmp.drop(tmp.head(1).index, inplace=True)  # remove column titles
+        tmp.columns = headers_pdf
+        assembled_table = pandas.concat([assembled_table, tmp])
+
+    # clean up unnecessary line returns in addresses
+    assembled_table = assembled_table.replace('\n', '', regex=True)
+    # add utility city column
+    assembled_table['Ville'] = "Lyon"
+
+    # save pdf data to csv
+    if csv_filename:
+        assembled_table.to_csv(csv_filename)
+
+    # add a location column with geocodes
+    assembled_table['location'] = add_geocode_to_dataset(
+        assembled_table,
+        geopy.geocoders.Nominatim(user_agent="assmat-prepare"))
+
+    # save again with geocodes
+    if csv_filename:
+        assembled_table.to_csv("{}_geocode.csv".format(csv_filename[:-4]))
+
+    # fix encoding for web visualisation
+    for field in ["Nom", "Prenom", "Adresse"]:
+        assembled_table["w" + field] = assembled_table[field].apply(
+            lambda x: repr(bytes(x, encoding="utf-16le"))[2:-1])
+
+    save_cache()
+    return assembled_table
+
+
+POPUP_TEMPLATE = ("<b>{wNom:s} {wPrenom!s}</b></br>"
+                  "{wAdresse}</br>"
+                  "<p><nobr>{Tel}</nobr></p>")
+
+
+def add_marker(row, fmap, icon, popup_template=POPUP_TEMPLATE):
+    """Callback function adding a marker to a folium map
+    row: pandas.Series
+         Dataframe row should contains a `location` field as a tuple of (lat, long)
+    fmap: folium.map
+         Will add markers to this map
+    icon:
+    popup_template: string template
+
+    """
+    if row["location"]:
+        folium.Marker([row["location"][0], row["location"][1]],
+                      popup=popup_template.format(**row),
+                      icon=icon).add_to(fmap)
+
+
+def create_map(data):
+    start_lat = 45.7452567
+    start_lng = 4.8416748
+    folium_map = folium.Map(location=[start_lat, start_lng],
+                            zoom_start=15,
+                            control_scale=True)
+
+    icon = None
+    data.apply(add_marker, axis=1, args=(
+        folium_map,
+        icon,
+    ))
+
+    return folium_map
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return create_map(DATA)._repr_html_()
+
 
 parser = optparse.OptionParser()
 parser.add_option(
@@ -22,77 +162,30 @@ parser.add_option(
     '-o',
     '--output',
     dest="out_filename",
-    default="out.csv",
+    default=None,
 )
 
-options, remainder = parser.parse_args()
-
-headers_pdf = ['Nom', 'Prenom', 'Adresse', 'Tel']
-
-tables = camelot.read_pdf(options.in_filename, pages='all')
-
-assembled_table = pandas.DataFrame()  #columns=headers_pdf)
-for table in tables[1:]:
-    print(table.parsing_report)
-    tmp = pandas.DataFrame(table.df)
-    if assembled_table.size == 0:  # we are dealing with the very first table
-        tmp.drop(tmp.head(1).index, inplace=True)  # remove column titles
-    tmp.columns = headers_pdf
-    assembled_table = pandas.concat([assembled_table, tmp])
-
-assembled_table = assembled_table.replace('\n', '', regex=True)
-assembled_table['Ville'] = "Lyon"
-
-assembled_table.loc[:, 'Nom'] = assembled_table['Nom'].apply(
-    lambda x: repr(bytes(x, encoding="utf-16le"))[2:-1])
-assembled_table.loc[:, 'Prenom'] = assembled_table['Prenom'].apply(
-    lambda x: repr(bytes(x, encoding="utf-16le"))[2:-1])
-
-print(assembled_table)
-
-assembled_table.to_csv(options.out_filename)
-
-geolocator = geopy.geocoders.Nominatim(user_agent="assmat-prepare")
-
-geocode = geopy.extra.rate_limiter.RateLimiter(geolocator.geocode,
-                                               min_delay_seconds=.1)
-
-df = assembled_table.copy()
-df['location'] = df.apply(
-    lambda row: geocode("{} {}".format(row["Adresse"], row["Ville"])), axis=1)
-df['point'] = df['location'].apply(lambda loc: tuple(loc.point)
-                                   if loc else None)
-
-df.to_csv("{}_geocode.csv".format(options.out_filename[:-4]))
-
-popup_template = ("<b>{Nom:s} {Prenom!s}</b></br>" "{Tel}")
-
-
-def add_marker(row, map, icon):
-    if row["point"]:
-        folium.Marker([row["point"][0], row["point"][1]],
-                      popup=popup_template.format(**row),
-                      icon=icon).add_to(map)
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    start_lat = 45.7434134
-    start_lng = 4.8509982
-    folium_map = folium.Map(location=[start_lat, start_lng],
-                            zoom_start=15,
-                            control_scale=True)
-
-    icon = None
-    df.apply(add_marker, axis=1, args=(
-        folium_map,
-        icon,
-    ))
-    return folium_map._repr_html_()
-
+parser.add_option("-s", "--save", dest="save_map", default=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    options, remainder = parser.parse_args()
+
+    GEOCODE_CACHE = pull_cache()
+    DATA = prepare_data_from_pdf(options.in_filename, options.out_filename)
+
+    #    DATA.loc[DATA['location'].isnull()] = add_geocode_to_dataset(
+    #        DATA.loc[DATA['location'].isnull()],
+    #        geopy.geocoders.ArcGIS(),
+    #        use_cache=False)
+
+    #DATA['locationarcgis'] = add_geocode_to_dataset(DATA,
+    #                                                geopy.geocoders.ArcGIS(),
+    #                                                use_cache=False)
+    #DATA.to_csv("/tmp/compare_geolocators.csv")
+
+    save_cache()
+
+    if options.save_map:
+        create_map(DATA).save(options.save_map)
+    else:
+        app.run(debug=True)
